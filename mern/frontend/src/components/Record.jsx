6422f544286e04bc36a821d16a586b77a5c8c8d3
Abhijith JB <jbabhi2@gmail.com>
2025-06-03 22:27:28 +0530

    testing

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -17,7 +17,7 @@ export default function Record() {
       if (!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://35.90.95.176:5050/record/${params.id.toString()}`
+        `http://backend-container:5050/record/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://35.90.95.176:5050/record", {
+        response = await fetch("http://backend-container:5050/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,13 +60,16 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://35.90.95.176:5050/record/${params.id}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(person),
-        });
+        response = await fetch(
+          `http://backend-container:5050/record/${params.id}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+          }
+        );
       }
 
       if (!response.ok) {
