e3838df0384f65aa74141b7545d5e2143026b449
Abhijith JB <jbabhi2@gmail.com>
2025-06-03 22:08:15 +0530

    mongo db changes

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = "mongodb://127.0.0.1:27017";
-// const URI = "mongodb://mongodb:27017";
+// const URI = "mongodb://127.0.0.1:27017";
+const URI = "mongodb://mongodb:27017";
 
 const client = new MongoClient(URI, {
   serverApi: {
