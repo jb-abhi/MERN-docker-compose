7ef91683c76e1beec59f243f0c7f639336cc5e8a
Abhijith JB <jbabhi2@gmail.com>
2025-06-06 12:54:35 +0530

    docker files

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch(`http://${API_URL}/record`, {
+        response = await fetch(`${API_URL}/record`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://${API_URL}/record/${params.id}`, {
+        response = await fetch(`${API_URL}/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
